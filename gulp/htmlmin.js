45f688c28f11eac0f4fabf22ff140fc0221c020d
feizheng <1290657123@qq.com>
2016-10-14 11:11:33 +0800

    finish

---
+++ gulp/htmlmin.js
@@ -0,0 +1,31 @@
+(function() {
+
+  'use strict';
+
+  var gulp = require('gulp');
+  var config = require('./config');
+  var argv = require('yargs').argv;
+  var $ = require('gulp-load-plugins')({
+    pattern: ['gulp-*', 'gulp.*', 'del']
+  });
+
+  //test
+  gulp.task('htmlmin', function() {
+    return gulp.src('test/justify.html')
+      .pipe($.htmlmin({
+        removeEmptyAttributes: true,
+        removeAttributeQuotes: true,
+        collapseBooleanAttributes: false,
+        collapseWhitespace: true,
+        removeComments:true,
+        ignoreCustomComments: [
+          /^\s+justify-start/,
+          /^\s+justify-end/,
+        ]
+      }))
+      .pipe($.rename({ extname: '.min.html'}))
+      .pipe(gulp.dest('test'));
+
+  });
+
+}());
